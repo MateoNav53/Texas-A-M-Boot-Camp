c45f89c1804804b081e1c2be9082e09b344bd88f
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-02-18 12:06:47 -0800

    formatting in moveForward function altered

--- Microsoft- DEV211.1x/HTML:CSS:Javascript Module Homework/js/script.js
+++ Microsoft- DEV211.1x/HTML:CSS:Javascript Module Homework/js/script.js
@@ -2,7 +2,7 @@ var slideInterval = 3500;
 function getFigures() {
     return document.getElementById('carousel').getElementsByTagName('figure');
 }
-function moveForward () {
+function moveForward() {
     var pointer;
     var figures = getFigures();
     for (var i = 0; i < figures.length; i++) {
