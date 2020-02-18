d57aadeb9594cb9a5e8f2275a39a2ce61633ab8d
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-02-18 12:23:21 -0800

    comment added

--- Microsoft- DEV211.1x/MOD4 Javascript Practice/js/script6.js
+++ Microsoft- DEV211.1x/MOD4 Javascript Practice/js/script6.js
@@ -2,9 +2,10 @@ var worker;
 function startWorker() {
     worker = new Worker('js/worker.js');
     worker.onmessage = function(event) {
-        document.getElementById("output").innerHTML += '<li>' + event.data + '</li>';
+        document.getElementById('output').innerHTML += '<li>' + event.data + '</li>';
     };
 }
 function stopWorker() {
     worker.terminate();
 }
+//It's not opening the worker file nor the js folder
