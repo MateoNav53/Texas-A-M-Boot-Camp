137ae881b1f40680145d5953c1bfd60474ecab4b
Mateo Navarrette <mateonavarrette@mateonattesmbp2.home>
2020-02-18 14:46:59 -0800

    comment added

--- Microsoft- DEV211.1x/HTML:CSS:Javascript Module Homework/js/script.js
+++ Microsoft- DEV211.1x/HTML:CSS:Javascript Module Homework/js/script.js
@@ -1,20 +1,20 @@
 var slideInterval = 3500;
 function getFigures() {
-    return document.getElementById('carousel').getElementsByTagName('figure');
+    return document.getElementById("carousel").getElementsByTagName("figure");
 }
 function moveForward() {
     var pointer;
     var figures = getFigures();
     for (var i = 0; i < figures.length; i++) {
-        if (figures[i].className = 'visible') {
-            figures[i].className = '';
+        if (figures[i].className = "visible") {
+            figures[i].className = "";
             pointer = i;
         }
     }
     if (++pointer == figures.length) {
         pointer = 0;
     }
-    figures[pointer].className = 'visible';
+    figures[pointer].className = "visible";
     setTimeout(moveForward, slideInterval);
 }
 function startPlayback() {
