e91cbdadb1125d0325a02d97ed6d90674336f825
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-02-21 11:34:54 -0800

    added more variables to manipulate DOM

--- Microsoft- DEV211.1x/MOD5 Adding External Libraries to Enhance HTML Apps/js/script.js
+++ Microsoft- DEV211.1x/MOD5 Adding External Libraries to Enhance HTML Apps/js/script.js
@@ -4,4 +4,9 @@ $(document).ready(function () {
 function manipulateDOM() {
     var h1Headers = $('h1');
     h1Headers.text('Hello World');
+    var h3Headers = $('h3');
+    h3Headers.css('color', '#37887D');
+    h3Headers.first().css('text-decoration', 'line-through');
+    var lastH3Header = $('h3:last');
+    lastH3Header.css('background-color', '#53226A');
 }
